668210b3a142075d18a8d8a08634b018a9798b38
tygrysku007 <51529405+tygrysku007@users.noreply.github.com>
2024-08-05 02:53:00 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'articubot_one'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'description'), glob('description/*')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='reeswan',
+    maintainer_email='reeswan@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
